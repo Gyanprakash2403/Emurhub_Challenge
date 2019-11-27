aa3a061006e5a8631da780ac23c6d2523b1bfcc5
GyanS <gyan.shrivastava@derivco.co.za>
2019-11-27 19:00:16 +0530

    dummy nav component creation for final test

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect} from 'react-redux-firebase';
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+
+const ProjectDerails = (props) => {  
+    
+    const { project, auth} = props;
+    if (!auth.uid) return <Redirect to='/signin' /> 
+    
+    if(project) {      
+        
+        return (
+        <div className='container section project-details'>
+            <div className='card z-depth-0'>
+                <div className='card-content'>
+                    <span className='cart-title'>{project.title}</span>
+                    <p>{project.content}</p>
+                </div>
+                <div className='card-action grey lighten-4 grey-text'>
+                    <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+                    <div>2nd November, 2am</div>
+                </div>
+            </div>
+        </div>
+        )
+    } else {
+        return(
+            <div className='container center'>
+                <p className='white-text'>Loading project...</p>
+            </div>
+        )
+        
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects
+    const project = projects ? projects[id] : null
+    return {
+        project : project,
+        auth: state.firebase.auth
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'projects'}
+    ])
+)(ProjectDerails)
---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,21 @@
+const createProject = (project) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // make async call to database
+        const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_TRANSACTION' });
+        }).catch(err => {
+            dispatch({ type: 'TRANSACTION_ERROR' }, err);
+        });
+    }
+};
+
+export default createProject;
